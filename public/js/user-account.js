a872610db674489ac46a28b900eb452c4cb3d3cc
Mel Mel Mom <melmelmomof4@gmail.com>
2024-03-08 20:48:41 -0500

    function to reload page with user times saved working

--- public/js/user-account.js
+++ public/js/user-account.js
@@ -1,35 +1,59 @@
-document.getElementById('saveBtn').addEventListener('click', async function() {
+document.getElementById('saveBtn').addEventListener('click', async function () {
   // Get the entered times
-  const wake= document.getElementById('wake').value;
+  const wake = document.getElementById('wake').value;
   const bTime = document.getElementById('eatB').value;
   const lTime = document.getElementById('eatL').value;
   const dTime = document.getElementById('eatD').value;
   const sleep = document.getElementById('sleep').value;
 
+
   // Save times users enter to localStorage
   const times = {
-      wake,
-      bTime,
-      lTime,
-      dTime,
-      sleep
+    wake,
+    bTime,
+    lTime,
+    dTime,
+    sleep
   };
 
-//call a db method
-const userData = await fetch('/user', {
-  method: 'PUT',
-  body: JSON.stringify ({times }),
-  headers: { 'Content-Type': 'application/json' }
-})
+
+  //call a db method
+  const userData = await fetch('/user', {
+    method: 'PUT',
+    body: JSON.stringify({ times }),
+    headers: { 'Content-Type': 'application/json' }
+  })
 
   localStorage.setItem('userTimes', JSON.stringify(times));
 
   alert('Times saved successfully!');
-  
+
 });
 
 
+function reloadUserData() {
+  // Retrieve the saved data from local storage
+  const userData = JSON.parse(localStorage.getItem('userTimes'));
 
-// write new function here and call it whenever they load the page
-// if localStorage.userTime = (parse_
-//   btime.value =)
+  // Get the entered times
+  const wake = document.getElementById('wake');
+  const bTime = document.getElementById('eatB');
+  const lTime = document.getElementById('eatL');
+  const dTime = document.getElementById('eatD');
+  const sleep = document.getElementById('sleep');
+  // Check if there is saved user data
+  if (userData) {
+    console.log(userData);
+
+    wake.value = userData.wake;
+    bTime.value = userData.bTime;
+    lTime.value = userData.lTime;
+    dTime.value = userData.dTime;
+    sleep.value = userData.sleep;
+  }
+}
+
+window.onload = function () {
+  reloadUserData();
+
+};
