da625a30ad0ce9804211cabbe8429c509884b6a7
Bryan Schneller <bryan.schneller@gmail.com>
2024-03-11 15:20:09 -0400

    Add medication model. Create blank medication routes js file

--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
 const User = require('./user')
 const Calendar = require('./calendar')
+const Medication = require('./medication')
 
 Calendar.belongsTo(User)
 User.hasMany(Calendar)
 
-module.exports = {User, Calendar}
+Medication.belongsTo(User)
+User.hasMany(Medication)
+
+module.exports = {User, Calendar, Medication}
