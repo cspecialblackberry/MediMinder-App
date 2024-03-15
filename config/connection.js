800dc9df9d5beb96feaf1d7cdad0baf4ba63dd2d
Hall Family <charliesamhall@icloud.com>
2024-03-14 21:47:06 -0400

    commit

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,26 @@
 const Sequelize = require('sequelize')
 require('dotenv').config()
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        port: process.env.DB_PORT,
-        logging: false   
-    }
-)
+let sequelize;
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  }else{
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            dialect: process.env.DB_DIALECT,
+            port: process.env.DB_PORT,
+            logging: false   
+        }
+    )
+  }
 
+
+//mysql://rgtxiub5b8zf62qa:sgfpmrf56cksqbxk@iu51mf0q32fkhfpl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/tw0sqgp9t2ff2k8t
+//first section dbuser
+//second password up to @
+//third db name up to us east
 module.exports = sequelize
